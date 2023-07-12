8911a87f07dcbabb7100fbb19736055a55ad0973
Mirko Pleli <mirko.pleli@outlook.de>
2023-07-12 09:39:20 +0200

    docs: add sample demo

---
+++ wsl2-warp-drive-win-dev/src/test/java/de/sybit/wsl2warpdrivewindev/Wsl2WarpDriveWinDevApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sybit.wsl2warpdrivewindev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wsl2WarpDriveWinDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
