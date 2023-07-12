77f245ffd9a5b57b206b6a6a9d508999040b6a40
Mirko Pleli <mirko.pleli@outlook.de>
2023-07-12 07:46:32 +0000

    docs: rename folder for demo; add controller

---
+++ demo/src/main/java/de/sybit/wsl2warpdrivewindev/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings to the visitors of the Java Forum! :)";
+	}
+}
